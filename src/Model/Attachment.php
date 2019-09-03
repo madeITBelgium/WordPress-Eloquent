e8b8415071edf92d27547651bee84a626103aa57
Made I.T <info@madeit.be>
2019-09-03 10:33:01 +0200

    Update Attachment.php

--- src/Model/Attachment.php
+++ src/Model/Attachment.php
@@ -4,8 +4,37 @@ namespace MadeITBelgium\WPEloquent\Model;
 
 class Attachment extends Post
 {
+    protected $appends = ['real_url'];
+
     public function post()
     {
         return $this->belongsTo(Post::class, 'post_parent', 'ID');
+    }
+
+    public function getRealUrlAttribute()
+    {
+        $filename = optional($this->meta()->where('meta_key', '_wp_attached_file')->first())->meta_value;
+        $filename = substr($filename, strripos($filename, '/') + 1);
+
+        $url = $this->guid;
+        $urlWithoutFile = substr($url, 0, stripos($url, '/'));
+
+        if (strpos($urlWithoutFile, '/wp-content') >= 0) {
+            $urlWithoutFile = substr($url, 0, strpos($url, '/wp-content'));
+        }
+
+        $thumb = '';
+        $metaData = unserialize(optional($this->meta()->where('meta_key', '_wp_attachment_metadata')->first())->meta_value);
+        if (strpos($metaData['sizes']['thumbnail']['file'], '/') === false) {
+            if (strpos($urlWithoutFile, 'wp-content') === false) {
+                $thumb = $urlWithoutFile.'/wp-content/uploads/'.$metaData['file'];
+            } else {
+                $thumb = $urlWithoutFile.'/'.$metaData['file'];
+            }
+        } else {
+            $thumb = $urlWithoutFile.'/'.$metaData['sizes']['thumbnail']['file'];
+        }
+
+        return $thumb;
     }
 }
