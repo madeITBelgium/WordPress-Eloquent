39f03b6c89df5d229d7676350481bdf9d30e12a5
Made I.T <info@madeit.be>
2019-10-23 13:26:17 +0200

    Update Comment.php

--- src/Model/Comment.php
+++ src/Model/Comment.php
@@ -12,7 +12,7 @@ class Comment extends \Illuminate\Database\Eloquent\Model
     protected $table = 'comments';
     protected $primaryKey = 'comment_ID';
     protected $fillable = [
-        'comment_agent', 'comment_approved', 'comment_author', 'comment_author_IP', 'comment_author_email', 'comment_author_url', 'comment_content', 'comment_date', 'comment_date_gmt', 'comment_karma', 'comment_parent', 'comment_post_ID', 'comment_type',
+        'comment_agent', 'comment_approved', 'comment_author', 'comment_author_IP', 'comment_author_email', 'comment_author_url', 'comment_content', 'comment_date', 'comment_date_gmt', 'comment_karma', 'comment_parent', 'comment_post_ID', 'comment_type', 'user_id',
     ];
     public $timestamps = false;
 
