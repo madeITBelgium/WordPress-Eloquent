d1113c749691e924ea570cd11b6c14183b830a06
Made I.T <info@madeit.be>
2020-02-11 10:00:41 +0100

    Update Meta.php

--- src/Model/User/Meta.php
+++ src/Model/User/Meta.php
@@ -8,7 +8,7 @@ class Meta extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'usermeta';
     public $timestamps = false;
-    protected $fillable = ['meta_key', 'meta_value'];
+    protected $fillable = ['meta_key', 'meta_value', 'user_id'];
     protected $primaryKey = 'umeta_id';
 
     public function user()
