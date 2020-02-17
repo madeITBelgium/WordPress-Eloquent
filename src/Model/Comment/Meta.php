2545984b41bee2791ad42202deadca9155ac0c6a
Made I.T <info@madeit.be>
2020-02-17 14:33:14 +0100

    Update Meta.php

--- src/Model/Comment/Meta.php
+++ src/Model/Comment/Meta.php
@@ -8,7 +8,7 @@ class Meta extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'commentmeta';
     public $timestamps = false;
-    protected $fillable = ['meta_key', 'meta_value'];
+    protected $fillable = ['meta_key', 'meta_value', 'comment_id'];
     protected $primaryKey = 'meta_id';
 
     public function comment()
